2ba909ccaf6623d8d4fa14bdf23a36ef40c8c1a2
Rahul Ohol <rahulohol01@gmail.com>
2023-07-24 12:41:35 +0530

    skills

---
+++ src/components/Exp/Skill.jsx
@@ -0,0 +1,31 @@
+import { Box, Flex, Text } from "@chakra-ui/react";
+import React from "react";
+import "./skill.css";
+export const Skill = ({ color, comp, reflink, name, icon }) => {
+  return (
+    <Box className="skill" w="max-content">
+      <a
+        href={reflink}
+        target="_blank"
+        rel="noreferrer"
+        style={{ "--clr": color }}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span>
+          <Flex flexDirection={"column"} justify="center" align={"center"}>
+            {comp || ""}
+            {icon ? <img src={icon} className="skillicon" alt="" /> : " "}
+            <Text fontFamily={"lora"} fontSize={["12px", "15px"]}>
+              {name}
+            </Text>
+          </Flex>
+        </span>
+      </a>
+    </Box>
+  );
+};
+
+export default Skill;
